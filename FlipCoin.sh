9b001c052a41161c70ac8ecdf9f23a9bb397e63f
Rajeshnalla21 <nallarajesh21@gmail.com>
2020-06-06 10:49:50 +0530

    FlipCoin Simaulation Problem

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,20 +1,54 @@
-#/bin/bash -x
+#!/bin/bash
 
-headTarget=0
-tailTarget=0
+echo Welcome to the coin flip program
 
-while [ $headTarget -ne 21 ] && [ $tailTarget -ne 21 ]
+#variables
+headWinCounter=0
+tailWinCounter=0
+
+
+while [[ headWinCounter -lt 21 && tailWinCounter -lt 21  ]]
 do
+	flip=$((RANDOM%2))
 
-                toss=$((RANDOM%2))
-                if [ $toss -eq 0 ]
-                then
-                        echo "Tails"
-                        tailTarget=$(($tailTarget+1))
-
-                else
-                        echo "Heads"
-                        headTarget=$(($headTarget+1))
-                fi
-                echo $headTarget $tailTarget
+	if [[ $flip -eq 1 ]]
+	then
+		((headWinCounter++))
+	else
+		((tailWinCounter++))
+	fi
 done
+
+if [[ $headWinCounter -eq $tailWinCounter ]]
+then
+	echo We have a tie. Rolling again........
+
+	while [[ $(($headWinCounter-$tailWinCounter)) -lt 2 && $(($tailWinCounter-$headWinCounter)) -lt 2  ]]
+	do
+   	flip=$((RANDOM%2))
+
+   	if [[ $flip -eq 1 ]]
+   	then
+      	((headWinCounter++))
+   	else
+      	((tailWinCounter++))
+   	fi
+	done
+
+	if [[ $headWinCounter -gt $tailWinCounter ]]
+	then
+   	echo HEADS has won by $((headWinCounter-tailWinCounter)) tosses
+	else
+   	echo TAILS has won by $((tailWinCounter-headWinCounter)) tosses
+	fi
+
+
+elif [[ $headWinCounter -gt $tailWinCounter ]]
+then
+	echo HEADS has won by $((headWinCounter-tailWinCounter)) tosses
+else
+	echo TAILS has won by $((tailWinCounter-headWinCounter)) tosses
+fi
+
+echo Number of times HEAD won: $headWinCounter
+echo Number of times TAIL won: $tailWinCounter
